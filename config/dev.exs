cd4a46b7cc974eb8c3944e8bb898b7e1c47ce2b1
Uriel Maldonado <urielmaldonado@zephir.attlocal.net>
2026-03-18 10:38:30 -0500

    fix: set dev server port back to 5000 to match Caddy reverse proxy

--- config/dev.exs
+++ config/dev.exs
@@ -28,7 +28,7 @@ config :eye_in_the_sky_web, EyeInTheSkyWeb.Repo,
 config :eye_in_the_sky_web, EyeInTheSkyWebWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [port: 5002],
+  http: [port: 5000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,

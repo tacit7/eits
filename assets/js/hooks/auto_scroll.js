b788822aacf2fc9538a44981b898809b421fb513
Uriel Maldonado <urielmaldonado@MacBookAir.attlocal.net>
2026-03-21 01:00:15 -0500

    fix: prevent auto_scroll from firing load_more_messages on initial mount

--- assets/js/hooks/auto_scroll.js
+++ assets/js/hooks/auto_scroll.js
@@ -2,13 +2,14 @@ export const AutoScroll = {
   mounted() {
     this.shouldAutoScroll = true
     this._loadingMore = false
+    this._mounted = false
 
     this._onScroll = () => {
       const { scrollHeight, scrollTop, clientHeight } = this.el
       this.shouldAutoScroll = scrollHeight - scrollTop - clientHeight <= 50
 
       // Auto-load older messages when scrolled near the top
-      if (!this._loadingMore && scrollTop < 100 && this.el.dataset.hasMore === "true") {
+      if (this._mounted && !this._loadingMore && scrollTop < 100 && this.el.dataset.hasMore === "true") {
         this._loadingMore = true
         this.pushEvent("load_more_messages", {})
       }
@@ -16,6 +17,7 @@ export const AutoScroll = {
 
     this.el.addEventListener("scroll", this._onScroll, { passive: true })
     this.scrollToBottom()
+    requestAnimationFrame(() => { this._mounted = true })
 
     this.handleEvent("new_message", () => {
       if (this.shouldAutoScroll) this.scrollToBottom()

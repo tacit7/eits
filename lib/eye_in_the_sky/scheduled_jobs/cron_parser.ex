b56328c88e96a8c94e7f89f322a155a8c98755b3
Uriel Maldonado <urielmaldonado@zephir.local>
2026-04-13 15:29:48 -0500

    fix: add workable_task to schema and validate timezone before bang functions
    - Add "workable_task" to validate_inclusion for job_type in ScheduledJob changeset
    - Add validate_timezone/1 to ScheduledJob changeset using DateTime.shift_zone/2 probe
    - Replace bang timezone functions in CronParser with non-bang versions wrapped in with/else
    - Log warning and return nil on invalid timezone instead of raising

--- lib/eye_in_the_sky/scheduled_jobs/cron_parser.ex
+++ lib/eye_in_the_sky/scheduled_jobs/cron_parser.ex
@@ -31,11 +31,19 @@ defmodule EyeInTheSky.ScheduledJobs.CronParser do
   defp next_cron_run_at(schedule_value, utc_now, timezone) do
     case Parser.parse(schedule_value) do
       {:ok, parsed} ->
-        local_now = utc_to_local(utc_now, timezone)
+        with {:ok, local_now} <- utc_to_local(utc_now, timezone),
+             {:ok, next_local} <- Crontab.Scheduler.get_next_run_date(parsed, local_now),
+             {:ok, next_utc_naive} <- local_to_utc(next_local, timezone),
+             {:ok, next_utc} <- DateTime.from_naive(next_utc_naive, "Etc/UTC") do
+          next_utc
+        else
+          {:error, :time_zone_not_found} ->
+            require Logger
+            Logger.warning("CronParser: invalid timezone #{inspect(timezone)}, skipping next_run_at")
+            nil
 
-        case Crontab.Scheduler.get_next_run_date(parsed, local_now) do
-          {:ok, next_local} -> local_to_utc(next_local, timezone) |> DateTime.from_naive!("Etc/UTC")
-          _ -> nil
+          _ ->
+            nil
         end
 
       {:error, _} ->
@@ -43,21 +51,33 @@ defmodule EyeInTheSky.ScheduledJobs.CronParser do
     end
   end
 
-  defp utc_to_local(naive_utc, "Etc/UTC"), do: naive_utc
+  defp utc_to_local(naive_utc, "Etc/UTC"), do: {:ok, naive_utc}
 
   defp utc_to_local(naive_utc, timezone) do
-    naive_utc
-    |> DateTime.from_naive!("Etc/UTC")
-    |> DateTime.shift_zone!(timezone)
-    |> DateTime.to_naive()
+    case DateTime.from_naive(naive_utc, "Etc/UTC") do
+      {:ok, dt} ->
+        case DateTime.shift_zone(dt, timezone) do
+          {:ok, shifted} -> {:ok, DateTime.to_naive(shifted)}
+          error -> error
+        end
+
+      error ->
+        error
+    end
   end
 
-  defp local_to_utc(naive_local, "Etc/UTC"), do: naive_local
+  defp local_to_utc(naive_local, "Etc/UTC"), do: {:ok, naive_local}
 
   defp local_to_utc(naive_local, timezone) do
-    naive_local
-    |> DateTime.from_naive!(timezone)
-    |> DateTime.shift_zone!("Etc/UTC")
-    |> DateTime.to_naive()
+    case DateTime.from_naive(naive_local, timezone) do
+      {:ok, dt} ->
+        case DateTime.shift_zone(dt, "Etc/UTC") do
+          {:ok, utc} -> {:ok, DateTime.to_naive(utc)}
+          error -> error
+        end
+
+      error ->
+        error
+    end
   end
 end

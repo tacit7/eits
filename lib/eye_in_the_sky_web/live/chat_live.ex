fd8fa14cb3d83ce7507b8068f405aef01335aa8f
Uriel Maldonado <urielmaldonado@MacBookAir.attlocal.net>
2026-04-19 05:18:29 -0500

    chat changes

--- lib/eye_in_the_sky_web/live/chat_live.ex
+++ lib/eye_in_the_sky_web/live/chat_live.ex
@@ -102,6 +102,11 @@ defmodule EyeInTheSkyWeb.ChatLive do
   end
 
   @impl true
+  def handle_event("set_notify_on_stop", %{"enabled" => enabled}, socket) do
+    {:noreply, assign(socket, :notify_on_stop, enabled in [true, "true", "on", 1, "1"])}
+  end
+
+  @impl true
   def handle_event("change_channel", %{"channel_id" => channel_id}, socket) do
     {:noreply, push_patch(socket, to: ~p"/chat?channel_id=#{channel_id}")}
   end

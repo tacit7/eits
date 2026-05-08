acd7e97edbc0533ff30bea2442a354ead4091a93
Uriel Maldonado <uriel@smpllabs.io>
2026-05-08 13:20:18 -0500

    credo: update file_editor_component_test to use FileHelpers.cm_language

--- test/eye_in_the_sky_web/components/file_editor_component_test.exs
+++ test/eye_in_the_sky_web/components/file_editor_component_test.exs
@@ -2,27 +2,27 @@
 defmodule EyeInTheSkyWeb.Components.FileEditorComponentTest do
   use ExUnit.Case, async: true
 
-  alias EyeInTheSkyWeb.Components.FileEditorComponent
+  alias EyeInTheSkyWeb.Helpers.FileHelpers
 
-  describe "infer_lang/1" do
+  describe "FileHelpers.cm_language/1" do
     test "elixir for .ex" do
-      assert FileEditorComponent.infer_lang("/foo/bar.ex") == "elixir"
+      assert FileHelpers.cm_language("/foo/bar.ex") == "elixir"
     end
 
     test "elixir for .exs" do
-      assert FileEditorComponent.infer_lang("/foo/bar.exs") == "elixir"
+      assert FileHelpers.cm_language("/foo/bar.exs") == "elixir"
     end
 
     test "shell for .sh" do
-      assert FileEditorComponent.infer_lang("/foo/bar.sh") == "shell"
+      assert FileHelpers.cm_language("/foo/bar.sh") == "shell"
     end
 
     test "text for unknown extension" do
-      assert FileEditorComponent.infer_lang("/foo/bar.toml") == "text"
+      assert FileHelpers.cm_language("/foo/bar.toml") == "text"
     end
 
     test "text for nil" do
-      assert FileEditorComponent.infer_lang(nil) == "text"
+      assert FileHelpers.cm_language(nil) == "text"
     end
   end
 end

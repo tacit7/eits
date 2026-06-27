c704bece40e5eedb18af64e6fec6fa9845369ad6
Uriel Maldonado <uriel@smpllabs.io>
2026-06-27 07:08:13 -0500

    fix: sanitize NotesTab HTML output with DOMPurify; add missing unique_constraints to 5 changesets

--- lib/eye_in_the_sky/projects/project.ex
+++ lib/eye_in_the_sky/projects/project.ex
@@ -37,6 +37,6 @@ defmodule EyeInTheSky.Projects.Project do
       :workspace_id
     ])
     |> validate_required([:name, :workspace_id])
-    |> unique_constraint(:path)
+    |> unique_constraint(:path, name: :projects_workspace_id_path_unique_index)
   end
 end
